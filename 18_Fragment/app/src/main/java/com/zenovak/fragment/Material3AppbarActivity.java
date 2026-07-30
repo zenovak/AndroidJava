7a8807ffa7ba73a198f634df698d2cce2391119b
Zenovak <xe.novak@outlook.com>
2026-07-30 18:04:54 +0800

    feat: added nav graph implementation example

--- 18_Fragment/app/src/main/java/com/zenovak/fragment/Material3AppbarActivity.java
+++ 18_Fragment/app/src/main/java/com/zenovak/fragment/Material3AppbarActivity.java
@@ -2,14 +2,21 @@ package com.zenovak.fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class Material3AppbarActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +29,12 @@ public class Material3AppbarActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        NavHostFragment frag = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentContainerView);
+        assert frag != null;
+        navController = frag.getNavController();
+
     }
 
     @Override
@@ -34,5 +47,13 @@ public class Material3AppbarActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.action_bar, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (NavigationUI.onNavDestinationSelected(item, navController)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
